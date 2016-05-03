fa6f4e6e83350d3785fe67561c776fa0acc4f6b8
Vladislav Khripunov <lokki.it.tech@gmail.com>
2016-05-03 09:01:53 +0000

    Update hello.py

--- static-1/hello.py
+++ static-1/hello.py
@@ -1,6 +1,6 @@
 from cgi import parse_qs
 
-def app(environ, start_response):
+def simple_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
