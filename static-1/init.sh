778263906768a1d1f9b4ce0bb3d2881072bd4f2c
Vladislav Khripunov <lokki.it.tech@gmail.com>
2016-05-03 08:46:35 +0000

    Update init.sh

--- static-1/init.sh
+++ static-1/init.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/web/etc
 cp ./etc/h.py ~/web/etc/gunicorn.conf
 cp ./etc/nginx.conf ~/web/etc/nginx.conf
+cp ./hello.py ~/web
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
